eda9693349cfe4547da21f61129877cc631802d4
aizul <aizulhussin@gmail.com>
2019-02-11 18:07:40 +0800

    add intended bug 3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,8 +22,14 @@ var b = "hello"
 
 var a = 7
 
-if(a=1){
+if(a==1){
     console.log("Intended bug to test if deepcode detect this")
+}
+
+
+var s = 'mystring';
+for (var i = 0; i < s.length; i++) {
+  console.log(s.charAt(i));
 }
 
 app.listen(3000, function () {
