b7b9ae6ef874a743a9f03ca4973b2c60b7c89957
aizul <aizulhussin@gmail.com>
2019-02-12 09:58:21 +0800

    add a===3 in test.js

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,6 +1,7 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const vhost = require('vhost')
-const serverReady = require('server-ready')
-const conf = require('../conf')
+var b = "7"
+
+var a = 7
+
+if(a===b){
+    console.log("a is b")
+}
